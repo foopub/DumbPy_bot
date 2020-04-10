4aa6d1f7f93a6fc67e17b1c6dea4fba1a1f6775d
somewillcallmeGod <krowlaxs@gmail.com>
2020-04-10 18:18:20 +0100

    Finally, working decorator. Checks also fixed to be only functions. Attempt to make subclass of member failed.

--- cogs/checks.py
+++ cogs/checks.py
@@ -3,21 +3,18 @@ from discord.ext import commands
 
 discord.Member.guild_permissions
 
-class Member(discord.Member):
-    def __init__(self):
-        pass
-     
-    def has_roles(author: discord.Member, roles: set) -> bool:
-        role_set = set()
-        for role in author.roles:
-            role_set.add(role.name)
-        return roles <= role_set
+def has_roles(author: discord.Member, roles: set) -> bool:
+    role_set = set()
+    for role in author.roles:
+        role_set.add(role.name)
+    return roles <= role_set
 
-    def has_perms(author: discord.Member, perms) -> bool:
-        pass
+def has_perms(author: discord.Member, perms) -> bool:
+    pass
 
-    def self_check(self, perms, roles):
-        return True
+def self_check(author: discord.Member, perms, roles) -> bool:
+    return True
 
-    def targer_check(self, target: discord.Member, perms, roles):
-        return True
+def target_check(author: discord.Member, 
+        target: discord.Member, perms, roles) -> bool:
+    return True
--- cogs/roles.py
+++ cogs/roles.py
@@ -2,54 +2,56 @@ from discord.ext import commands
 import discord
 from cogs import checks 
 import functools
+
+def give_role(
+    
+        _funct=None,
+        role_type: str=None, 
+        cleanup: str=None,      #'hard' deletes role globally 
+        perms: set={'manage_roles'}, 
+        roles: set={'Mod'}
+        ) -> None:
+
+    def funct_wrapper(role_function):
+        @functools.wraps(role_function)
+        async def my_wrapper(
+            self,
+            context: commands.Context,
+            role_name: str,
+            member: discord.Member=None,
+            ) -> None:
+            if not member:      #make author target if none specified
+                member = context.author
+                if checks.self_check(member, perms, roles): 
+                    #can author do this to themselves?
+                    pass
+            elif checks.target_check(member, context.author, perms, roles): 
+                #can author do this to others?
+                pass
+            else:
+                await context.send("You're not allowed to do this >:(")
+                return None
+            
+            print('\nIside wrapper', self, context, role_name, member)
+            new_role = await role_function(self, context, role_name, member)
+
+            if cleanup:
+                for role in member.roles:
+                    if str(role).split('_')[0] == role_type:
+                        await member.remove_roles(role)
+                        if cleanup == 'hard' and not role.members:
+                            await role.delete(reason='Role replaced')
+                await member.add_roles(new_role)
+        return my_wrapper 
+    if _funct is None:
+        return funct_wrapper
+    else:
+        return funct_wrapper(_funct)
+
 class Roles(commands.Cog):
 
     def __init__(self, client: commands.Bot) -> None:
         self.client = client
-
-    def give_role(
-        
-            _funct=None,
-            role_type: str=None, 
-            cleanup: str=None,      #'hard' deletes role globally 
-            perms: set={'manage_roles'}, 
-            roles: set={'Mod'}
-            ) -> None:
-
-        def funct_wrapper(role_function):
-
-            async def wrapper(
-                context: commands.Context,
-                role_name: str,
-                member: checks.Member=None,
-                ) -> None:
-
-                if not member:      #make author target if none specified
-                    member = await checks.Member(context.author)
-                    if await member.self_check(perms, roles): 
-                        #can author do this to themselves?
-                        pass
-                elif await member.targer_check(context.author, perms, roles): 
-                    #can author do this to others?
-                    pass
-                else:
-                    await context.send("You're not allowed to do this >:(")
-                    return None
-                
-                new_role = await role_function(context, role_name, member)
-
-                if cleanup:
-                    for role in member.roles:
-                        if str(role).split('_')[0] == role_type:
-                            await member.remove_roles(role)
-                            if cleanup == 'hard' and not role.members:
-                                await role.delete(reason='Role replaced')
-                    await member.add_roles(new_role)
-
-        if _funct is None:
-            return funct_wrapper
-        else:
-            return funct_wrapper(_funct)
 
     @commands.command(name='colour')
     @give_role(cleanup='hard',role_type='colour')
@@ -60,6 +62,7 @@ class Roles(commands.Cog):
             member: discord.Member=None
             ) -> discord.Role:
         
+        print('\nInside colour', self, context, colour_hex, member)
         role_name = f'colour_{colour_hex}'
 
         try:
@@ -67,10 +70,9 @@ class Roles(commands.Cog):
                     name=role_name, 
                     colour=discord.Colour(int(colour_hex, base=16))
                     )
-            await context.send('Swag granted.')
         except ValueError:
             await context.send(f"{colour_hex} isn't a valid hex you retard!")
-            return new_role       
+        return new_role       
 
     @commands.command(name='clean_roles')
     async def clean_roles(self, context: commands.Context) -> None:
