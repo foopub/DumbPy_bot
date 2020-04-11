fd08c4a749975ba5e61dca47f836d2d0a6b16c96
somewillcallmeGod <krowlaxs@gmail.com>
2020-04-11 21:43:59 +0100

    Added checks

--- DumbMain.py
+++ DumbMain.py
@@ -1,7 +1,8 @@
 from os import getenv      
 from discord.ext import commands      
 from dotenv import load_dotenv      
-import discord      
+import discord
+from cogs.sup import checks
       
 load_dotenv()      
 TOKEN = getenv('DISCORD_TOKEN')
@@ -9,11 +10,17 @@ TOKEN = getenv('DISCORD_TOKEN')
 client = commands.Bot(command_prefix='.')
 
 all_cogs = getenv('ALL_COGS') 
+
 @client.event      
 async def on_ready():      
     print(f'{client.user} connected!') 
 
-@client.command()
+@client.check
+async def bot_banned(context: commands.Context) -> bool:
+    return 'bot_banned' not in [i.name for i in context.author.roles]
+
+@client.command(name='load')
+@commands.is_owner()
 async def load(context: commands.Context, *, names: str) -> None:
     """
     Load modules. Names separated by a single space.
@@ -28,8 +35,10 @@ async def load(context: commands.Context, *, names: str) -> None:
             await context.send(f'Module {name.title()} loaded!')
         except:
             await context.send(f'Could not load {name.title()}!')
+            raise
 
-@client.command()
+@client.command(name='unload')
+@commands.is_owner()
 async def unload(context: commands.Context, *, names: str) -> None:
     """
     Unload modules. Names separated by a single space.
@@ -44,8 +53,10 @@ async def unload(context: commands.Context, *, names: str) -> None:
             await context.send(f'Module {name.title()} unloaded!')
         except:
             await context.send(f'Could not unload {name.title()}!')
+            raise
 
-@client.command()
+@client.command(name='reload')
+@commands.is_owner()
 async def reload(context: commands.Context, *, names: str) -> None:
     """
     Reload modules. Names separated by a single space.
@@ -60,6 +71,7 @@ async def reload(context: commands.Context, *, names: str) -> None:
             client.load_extension(f'cogs.{name.lower()}')
             await context.send(f'Module {name.title()} reloaded!')
         except:
-            await context.send('Could not reload {name.title()}!')
+            await context.send(f'Could not reload {name.title()}!')
+            raise
 
 client.run(TOKEN)
