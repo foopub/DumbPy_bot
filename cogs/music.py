70af5b6735700901de3e87dc68b376088d51fd1f
somewillcallmeGod <krowlaxs@gmail.com>
2020-04-18 17:06:57 +0100

    Starting an attempt at music module

---
+++ cogs/music.py
@@ -0,0 +1,27 @@
+from discord.ext import commands
+import discord
+
+class Music(commands.Cog):
+    def __init__(self, client: commands.Bot) -> None:
+        self.client = client
+
+    @commands.command(name='music')
+    async def music(self, context: commands.Context):
+        pass
+
+    @commands.group()
+    async def mu(self, context: commands.Context):
+        pass
+    
+    @mu.command()
+    async def play(self, context: commands.Context):
+        self.vclient = await context.author.voice.channel.connect()
+        pass
+
+    @mu.command()
+    async def leave(self, context: command.Context):
+        await self.vclient.disconnect()
+
+def setup(client: commands.Bot) -> None:
+    client.add_cog(Music(client))
+    
